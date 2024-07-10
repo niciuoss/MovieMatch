76bf2227345d65f74f472049e67f4bc7cba8c9ba
Vinicius Sales <viniciusamorimsales@gmail.com>
2024-07-10 15:29:09 -0300

    front e back corrigidos

--- back-end/server.js
+++ back-end/server.js
@@ -20,10 +20,32 @@ app.get('/api/categories', async (req, res) => {
   }
 });
 
+app.get('/api/recommendations', async (req, res) => {
+  const genreIds = req.query.genreIds;
+
+  if (!genreIds) {
+    return res.status(400).send('Genres parameter is required');
+  }
+
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+      params: {
+        api_key: process.env.TMDB_API_KEY,
+        with_genres: genreIds,
+        language: 'pt-BR',
+      },
+    });
+    res.json(response.data.results);
+  } catch (error) {
+    console.error('Error fetching recommendations:', error);
+    res.status(500).send('Error fetching recommendations');
+  }
+});
+
 app.get('/api/highlights', async (req, res) => {
   try {
     const response = await axios.get(`https://api.themoviedb.org/3/movie/now_playing`, {
-      params: { api_key: process.env.TMDB_API_KEY, language: 'en-US', page: 1 },
+      params: { api_key: process.env.TMDB_API_KEY, language: 'pt-BR', page: 1 },
     });
     res.json(response.data.results);
   } catch (error) {
@@ -34,34 +56,11 @@ app.get('/api/highlights', async (req, res) => {
 app.get('/api/trending', async (req, res) => {
   try {
     const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week`, {
-      params: { api_key: process.env.TMDB_API_KEY },
+      params: { api_key: process.env.TMDB_API_KEY, language: 'pt-BR' },
     });
     res.json(response.data.results);
   } catch (error) {
-    console.error('Failed to fetch trending movies:', error);
     res.status(500).json({ error: 'Failed to fetch trending movies' });
-  }
-});
-
-router.get('/recommendations', async (req, res) => {
-  const genreIds = req.query.genreIds;
-
-  if (!genreIds) {
-    return res.status(400).send('Genres parameter is required');
-  }
-
-  try {
-    const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
-      params: {
-        api_key: 'c38a7e8f0fffa6d905b202e01ad8f27b',
-        with_genres: genreIds,
-      },
-    });
-
-    res.json(response.data.results);
-  } catch (error) {
-    console.error('Error fetching recommendations:', error);
-    res.status(500).send('Error fetching recommendations');
   }
 });
 
--- front-end/src/pages/app/home.tsx
+++ front-end/src/pages/app/home.tsx
@@ -1,23 +1,42 @@
-import { Badge } from '@/components/ui/badge';
-import { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet-async';
-import axios from 'axios';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from '@/components/ui/button';
-import { Play } from 'lucide-react';
-import { Separator } from '@/components/ui/separator';
+import { Badge } from '@/components/ui/badge'
+import { useState, useEffect } from 'react'
+import { Helmet } from 'react-helmet-async'
+import axios from 'axios'
+import { FaRegHeart, FaHeart } from "react-icons/fa"
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle 
+} from "@/components/ui/card"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import { Play } from 'lucide-react'
+import { Separator } from '@/components/ui/separator'
 
 export function Home() {
   const [categories, setCategories] = useState<Array<{ id: number; name: string }>>([]);
   const [selectedCategories, setSelectedCategories] = useState<Array<number>>([]);
-  const [highlights, setHighlights] = useState<Array<{ id: number; title: string; poster_path: string }>>([]);
-  const [trendingMovies, setTrendingMovies] = useState<Array<{ id: number; title: string; poster_path: string }>>([]);
-  const [recommendations, setRecommendations] = useState<Array<{ id: number; title: string; poster_path: string }>>([]);
+  const [highlights, setHighlights] = useState<Array<{ id: number; title: string; poster_path: string; overview: string  }>>([]);
+  const [trendingMovies, setTrendingMovies] = useState<Array<{ id: number; title: string; poster_path: string; overview: string }>>([]);
+  const [recommendations, setRecommendations] = useState<Array<{ id: number; title: string; poster_path: string; overview: string; genre_ids: number[] }>>([]);
+  const [genreMap, setGenreMap] = useState<{ [key: number]: string }>({});
+  const [favoriteMovies, setFavoriteMovies] = useState(new Set());
+  const [movieVideos, setMovieVideos] = useState<{ [key: number]: { key: string, site: string } | null }>({});
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const response = await axios.get('/api/categories'); 
+        const response = await axios.get('/api/categories');
         setCategories(response.data);
       } catch (error) {
         console.error('Error fetching categories:', error);
@@ -26,7 +45,7 @@ export function Home() {
 
     const fetchHighlights = async () => {
       try {
-        const response = await axios.get('/api/highlights'); 
+        const response = await axios.get('/api/highlights');
         setHighlights(response.data);
       } catch (error) {
         console.error('Error fetching highlights:', error);
@@ -35,22 +54,37 @@ export function Home() {
 
     const fetchTrendingMovies = async () => {
       try {
-        const response = await axios.get('/api/trending'); 
+        const response = await axios.get('/api/trending');
         setTrendingMovies(response.data);
       } catch (error) {
         console.error('Error fetching trending movies:', error);
       }
     };
 
+    const fetchGenres = async () => {
+      try {
+        const response = await axios.get('/api/genres');
+        const genres = response.data;
+        const genreMap = genres.reduce((acc: { [key: number]: string }, genre: { id: number; name: string }) => {
+          acc[genre.id] = genre.name;
+          return acc;
+        }, {});
+        setGenreMap(genreMap);
+      } catch (error) {
+        console.error('Error fetching genres:', error);
+      }
+    };
+
     fetchCategories();
     fetchHighlights();
     fetchTrendingMovies();
+    fetchGenres();
   }, []);
 
   useEffect(() => {
     const fetchRecommendations = async () => {
       if (selectedCategories.length === 0) {
-        setRecommendations([]); 
+        setRecommendations([]);
         return;
       }
 
@@ -71,12 +105,41 @@ export function Home() {
     fetchRecommendations();
   }, [selectedCategories]);
 
+  useEffect(() => {
+    const fetchAllVideos = async () => {
+      const videosMap = {};
+      for (const movie of recommendations) {
+        const video = await fetchMovieVideos(movie.id);
+        videosMap[movie.id] = video;
+      }
+      setMovieVideos(videosMap);
+    };
+
+    fetchAllVideos();
+  }, [recommendations]);
+
   const handleCategorySelect = (id: number) => {
     setSelectedCategories((prevSelected) =>
       prevSelected.includes(id)
         ? prevSelected.filter((c) => c !== id)
         : [...prevSelected, id]
     );
+  };
+
+  const toggleFavorite = (movieId) => {
+    setFavoriteMovies((prevFavorites) => {
+      const newFavorites = new Set(prevFavorites);
+      if (newFavorites.has(movieId)) {
+        newFavorites.delete(movieId);
+      } else {
+        newFavorites.add(movieId);
+      }
+      return newFavorites;
+    });
+  };
+
+  const getGenreNames = (genreIds: number[]) => {
+    return genreIds.map(id => genreMap[id]).filter(Boolean).join(', ');
   };
 
   return (
@@ -90,9 +153,7 @@ export function Home() {
           <Badge
             key={category.id}
             onClick={() => handleCategorySelect(category.id)}
-            className={`cursor-pointer ${
-              selectedCategories.includes(category.id) ? 'bg-primary text-white' : 'bg-gray-300 text-black'
-            } w-[5.4rem] h-[2.5rem] flex items-center justify-center`}
+            className={`cursor-pointer ${selectedCategories.includes(category.id) ? 'bg-primary text-white' : 'bg-gray-300 text-black'} w-[5.4rem] h-[2.5rem] flex items-center justify-center`}
           >
             {category.name}
           </Badge>
@@ -102,29 +163,57 @@ export function Home() {
       <div className="flex flex-wrap gap-4 mt-4">
         {recommendations.map((movie) => (
           <Card key={movie.id} className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 flex flex-col">
-            <CardHeader>
+            <CardHeader className="relative">
+              <div className='absolute top-2 right-2 cursor-pointer' onClick={() => toggleFavorite(movie.id)}>
+              {favoriteMovies.has(movie.id) ? <FaHeart className='text-red-500' /> : <FaRegHeart className='text-gray-500' />}
+              </div>             
               <CardTitle>
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full h-auto rounded-md" />
               </CardTitle>
               <CardDescription>
-                <p>{movie.genre_ids}</p>
+                <p>{getGenreNames(movie.genre_ids)}</p>
               </CardDescription>
             </CardHeader>
             <CardContent className='flex-grow font-semibold text-xl'>
               {movie.title}
             </CardContent>
             <CardFooter className="mt-auto">
-              <Button className='gap-2 w-full justify-center'>
-                Trailer ou outros detalhes
-                <Play/>
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className='gap-2 w-full justify-center'>
+                    Trailer ou sinopse
+                    <Play />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{movie.title}</DialogTitle>
+                    <DialogDescription>
+                      {movie.overview}
+                      {movieVideos[movie.id] ? (
+                        <iframe 
+                          width="100%" 
+                          height="315" 
+                          src={`https://www.youtube.com/embed/${movieVideos[movie.id].key}`} 
+                          title="YouTube video player" 
+                          frameBorder="0" 
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                          allowFullScreen>
+                        </iframe>
+                      ) : (
+                        <p>Trailer não disponível</p>
+                      )}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
             </CardFooter>
           </Card>
         ))}
       </div>
 
       <div className='pt-10 lg:w-[71.45rem]'>
-        <Separator/>
+        <Separator />
       </div>
 
       <h2 className='text-2xl font-bold mt-8'>Destaques</h2>
@@ -133,27 +222,52 @@ export function Home() {
           <Card key={movie.id} className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 flex flex-col">
             <CardHeader>
               <CardTitle>
-              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full h-auto rounded-md" />
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full h-auto rounded-md" />
               </CardTitle>
               <CardDescription>
-                <p>{movie.genre_ids}</p>
+                <p>{getGenreNames(movie.genre_ids)}</p>
               </CardDescription>
             </CardHeader>
             <CardContent className='flex-grow font-semibold text-xl'>
               {movie.title}
             </CardContent>
             <CardFooter className="mt-auto">
-              <Button className='gap-2 w-full justify-center'>
-                Trailer ou outros detalhes
-                <Play/>
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className='gap-2 w-full justify-center'>
+                    Trailer ou sinopse
+                    <Play />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{movie.title}</DialogTitle>
+                    <DialogDescription>
+                      {movie.overview}
+                      {movieVideos[movie.id] ? (
+                        <iframe 
+                          width="100%" 
+                          height="315" 
+                          src={`https://www.youtube.com/embed/${movieVideos[movie.id].key}`} 
+                          title="YouTube video player" 
+                          frameBorder="0" 
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                          allowFullScreen>
+                        </iframe>
+                      ) : (
+                        <p>Trailer não disponível</p>
+                      )}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
             </CardFooter>
           </Card>
         ))}
       </div>
 
       <div className='pt-10 lg:w-[71.45rem]'>
-        <Separator/>
+        <Separator />
       </div>
 
       <h2 className='text-2xl font-bold mt-8'>Filmes em Alta</h2>
@@ -165,17 +279,43 @@ export function Home() {
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-full h-auto rounded-md" />
               </CardTitle>
               <CardDescription>
-                <p>{movie.genre_ids}</p>
+                <p>{getGenreNames(movie.genre_ids)}</p>
               </CardDescription>
             </CardHeader>
             <CardContent className='flex-grow font-semibold text-xl'>
               {movie.title}
             </CardContent>
             <CardFooter className="mt-auto">
-              <Button className='gap-2 w-full justify-center'>
-                Trailer ou outros detalhes
-                <Play/>
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className='gap-2 w-full justify-center'>
+                    Trailer ou sinopse
+                    <Play />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>{movie.title}</DialogTitle>
+                    <DialogDescription>
+                      {movie.overview}
+                      <Separator/>
+                      {movieVideos[movie.id] ? (
+                        <iframe 
+                          width="100%" 
+                          height="315" 
+                          src={`https://www.youtube.com/embed/${movieVideos[movie.id].key}`} 
+                          title="YouTube video player" 
+                          frameBorder="0" 
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                          allowFullScreen>
+                        </iframe>
+                      ) : (
+                        <p>Trailer não disponível</p>
+                      )}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
             </CardFooter>
           </Card>
         ))}
